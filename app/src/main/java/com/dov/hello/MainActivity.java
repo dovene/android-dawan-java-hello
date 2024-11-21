0a5ffaf69198889378589fa0e7e163ce208cc61f
dovene <adovene@gmail.com>
2024-11-21 20:04:51 +0100

    hello

--- app/src/main/java/com/dov/hello/MainActivity.java
+++ app/src/main/java/com/dov/hello/MainActivity.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText lastnameEdit;
     private TextView greetText;
     private Button greetButton;
-    private Button toastButton;
-    private Button snackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +23,9 @@ public class MainActivity extends AppCompatActivity {
         lastnameEdit = findViewById(R.id.lastname_edit);
         greetText = findViewById(R.id.greet_text);
         greetButton = findViewById(R.id.greet_button);
-        toastButton = findViewById(R.id.toast_button);
-        snackButton = findViewById(R.id.snack_button);
 
         greetButton.setOnClickListener(v -> {
             greetText.setText("Bonjour " + firstnameEdit.getText().toString() + " " + lastnameEdit.getText().toString() + " !");
-        });
-
-
-        toastButton.setOnClickListener(v -> {
-            Toast.makeText(this, "Bonjour", Toast.LENGTH_SHORT).show();
-
-        });
-
-        snackButton.setOnClickListener(v -> {
-            Snackbar.make(snackButton, "Bonjour et aurevoir", Snackbar.LENGTH_LONG).setAction("Aurevoir", view -> {
-                finish();
-            }).show();
         });
 
     }
