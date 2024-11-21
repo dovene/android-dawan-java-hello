5b36d9bc9c59dd2bd7cf841946d8db24c9451b41
dovene <adovene@gmail.com>
2024-11-21 20:02:37 +0100

    add toast and snackbar

--- app/src/main/java/com/dov/hello/ContentProviderClient.java
+++ app/src/main/java/com/dov/hello/ContentProviderClient.java
@@ -8,10 +8,8 @@ import android.widget.Toast;
 public class ContentProviderClient {
 
     private void readFromOtherApp(Context context) {
-        // Define the URI of the ContentProvider
         Uri contentUri = Uri.parse("content://com.dov.calculator.provider/calculator");
 
-        // Query example
         Cursor cursor = context.getContentResolver().query(
                 contentUri,
                 null,  // all columns
@@ -20,7 +18,6 @@ public class ContentProviderClient {
                 null   // no sort order
         );
 
-// Process the results
         if (cursor != null) {
             while (cursor.moveToNext()) {
                 int id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
@@ -33,19 +30,4 @@ public class ContentProviderClient {
         }
     }
 
-
-
-    /*
-    *
-        Toast.makeText(this, "Bonjour", Toast.LENGTH_SHORT).show();
-
-        Snackbar.make(greetButton, "Bonjour et aurevoir", Snackbar.LENGTH_LONG).setAction("Aurevoir", v -> {
-            finish();
-        }).show();
-
-        Intent intent = getIntent();
-        intent.putExtra("firstname", firstnameEdit.getText().toString());
-        Log.d("OnCreate", "Bienvenue");
-
-        startActivity(new Intent(MainActivity.this, SecondActivity.class));*/
 }
--- app/src/main/java/com/dov/hello/MainActivity.java
+++ app/src/main/java/com/dov/hello/MainActivity.java
@@ -1,9 +1,6 @@
 package com.dov.hello;
 
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,12 +8,16 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
     private EditText firstnameEdit;
     private EditText lastnameEdit;
     private TextView greetText;
     private Button greetButton;
+    private Button toastButton;
+    private Button snackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +28,23 @@ public class MainActivity extends AppCompatActivity {
         lastnameEdit = findViewById(R.id.lastname_edit);
         greetText = findViewById(R.id.greet_text);
         greetButton = findViewById(R.id.greet_button);
+        toastButton = findViewById(R.id.toast_button);
+        snackButton = findViewById(R.id.snack_button);
 
         greetButton.setOnClickListener(v -> {
             greetText.setText("Bonjour " + firstnameEdit.getText().toString() + " " + lastnameEdit.getText().toString() + " !");
+        });
+
+
+        toastButton.setOnClickListener(v -> {
+            Toast.makeText(this, "Bonjour", Toast.LENGTH_SHORT).show();
+
+        });
+
+        snackButton.setOnClickListener(v -> {
+            Snackbar.make(snackButton, "Bonjour et aurevoir", Snackbar.LENGTH_LONG).setAction("Aurevoir", view -> {
+                finish();
+            }).show();
         });
 
     }
